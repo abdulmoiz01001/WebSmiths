11058b85233d04cdc227bb975a5298b45c706150
abdulmoiz01001 <profession.ai01001@hotmail.com>
2024-09-07 11:00:29 +0500

    pull and developer changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeLayout from './pages/HomeLayout';
 import AdminLayout from './pages/AdminLayout';
-<<<<<<< HEAD
 import RegistrationPage from './pages/RegistrationPage';
-=======
 import OtpVerifyPage from './pages/OtpVerifyPage';
 import LoginPage from './pages/LoginPage';
 import StatisticsPage from './pages/StatisticsPage';
 import DoctorsPage from './pages/DoctorsPage';
 import PatientsPage from "./pages/PatientsPage";
 import AppointmentsPage from "./pages/AppointmentsPage";
-import RegistrationPage from "./pages/RegistrationPage";
 import RegistrationRequestsPage from "./pages/RegistrationRequestsPage";
 import MedicalRecordsPage from "./pages/MedicalRecordsPage";
->>>>>>> c7609eab09420d92e883d1ad901c93c4ca5fcc5e
 
 function App() {
 
@@ -24,8 +20,8 @@ function App() {
     <Routes>
       <Route path="/otp-verify" element={<OtpVerifyPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<HomeLayout />} >
       <Route path='/register' element={<RegistrationPage />} />
+      <Route path="/" element={<HomeLayout />} >
       </Route>
       <Route path="/admin" element={<AdminLayout />} >
       <Route path="statistics" element={<StatisticsPage />} />
--- client/src/pages/RegistrationPage.tsx
+++ client/src/pages/RegistrationPage.tsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import RegistrationComp from '../components/RegistrationComp'
-=======
-
->>>>>>> c7609eab09420d92e883d1ad901c93c4ca5fcc5e
 
 const RegistrationPage = () => {
   return (
